0bc3289ad78c69083bb1579549fb4a0c91c3ce9c
FrancescoZinnato <147512111+FrancescoZinnato@users.noreply.github.com>
2024-03-25 19:21:45 +0100

    Caricamento partita dal nuovo carosello in Home

--- java/start/controller/PartitaController.java
+++ java/start/controller/PartitaController.java
@@ -2,9 +2,14 @@ package start.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpSession;
 import start.dao.PartitaDAO;
+import start.model.Partita;
+import start.model.Utente;
 
 @Controller
 @RequestMapping("/")
@@ -12,5 +17,14 @@ public class PartitaController {
 	@Autowired
 	PartitaDAO service;
 	
-	//
+	@GetMapping("/caricaDaHome/{nomePartita}")
+	public String caricaDaHome(@PathVariable String nomePartita, HttpSession session) {
+		Utente utente = (Utente) session.getAttribute("utente");
+		if(utente == null) {
+			return "redirect:/login";
+		}
+		Partita partita = service.recuperaPartitaNomeUtente(nomePartita, utente);
+		session.setAttribute("partita", partita);
+		return "redirect:/gioca";
+	}
 }
